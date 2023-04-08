030bcaad8b272cee7fb4c8233df0b38ad31edccb
Fermin <ferc@outlook.com>
2023-04-08 20:52:32 -0300

    facturasystem site

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#2e58a2",
+          500: "#325fad",
+          600: "#295095",
+          700: "#1d4ed8",
+          800: "#1e3c72",
+          900: "#1e3a8a",
+        },
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+  //darkMode: 'media'
+};
